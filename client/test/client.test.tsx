e0967142bd414a478d7afc1824f27fbca7d8ab74
Alfredo Chavez <alfredocharom@gmail.com>
2025-11-17 17:55:45 +0100

    feat(client.test): start testing the client
    Started adding simple test to the client using Vitest. So far we have implemented tests for the
    Footer component.

--- client/test/client.test.tsx
+++ client/test/client.test.tsx
@@ -1,11 +1,16 @@
 import { describe, it, expect } from 'vitest';
-import React from 'react';
+// import React from 'react';
 import { render } from '@testing-library/react';
-import App from '../src/App.tsx';
+import Footer from '../src/components/Footer.tsx';
+// import App from '../src/App.tsx';
+// import HomePage from '../src/pages/HomePage.tsx';
 
 describe.only('App component', () => {
   it('renders without crashing', () => {
-    const { container } = render(<App />);
+    const { container } = render(<Footer />);
     expect(container).toBeTruthy();
   });
+  // it('1+1', () => {
+  //   expect(1+1).toBe(2);
+  // });
 });
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vitest/config";
+import react from '@vitejs/plugin-react';
+
 // import dotenv from "dotenv";
 
 export default defineConfig({
+  plugins: [react()],
   test: {
+    environment: 'jsdom',
     globals: true,
     setupFiles: ["./test/setup.ts"],
     coverage:{
